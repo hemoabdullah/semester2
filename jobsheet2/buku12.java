ec221f24d68885bb4d1b8be3bd1167c6f7884300
hemoabdullah <144300459+hemoabdullah@users.noreply.github.com>
2024-06-03 01:55:14 +0700

    Add files via upload

---
+++ jobsheet2/buku12.java
@@ -0,0 +1,41 @@
+package jobsheet2;
+
+public class buku12 {
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilInformasi() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang :" + pengarang);
+        System.out.println("Jumlah halaman :" + halaman);
+        System.out.println("Sisa stok:" + stok);
+        System.out.println("Harga: Rp" + harga);
+    }
+
+    void terjual(int jml) {
+        if (stok > 0) {
+            stok -= jml;
+        } else {
+            System.out.println("Stok habis");
+        }
+    }
+
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+
+    public buku12() {
+    }
+
+    public buku12(String jud, String pg, int hal, int stok, int har) {
+        judul = jud;
+        pengarang = pg;
+        halaman = hal;
+        this.stok = stok;
+        harga = har;
+    }
+}
